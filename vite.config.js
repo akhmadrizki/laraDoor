3555fcf3d397b4a96daee84042db90f071f521a3
Akhmad Rizki Prayoga <akhmadrizki24@gmail.com>
2022-10-20 15:07:24 +0800

    Configure vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig, loadEnv } from "vite";
+import laravel from "laravel-vite-plugin";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-});
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    const https = {
+        key: process.env.VITE_DEV_SERVER_KEY,
+        cert: process.env.VITE_DEV_SERVER_CERT,
+    };
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: ["resources/sass/app.scss", "resources/js/app.js"],
+                refresh: true,
+            }),
+        ],
+        server: {
+            https: process.env.VITE_DEV_SERVER_KEY ? https : false,
+            host: process.env.VITE_DEV_SERVER || "localhost",
+        },
+    });
+};
